1bda172a8a42872193f6a5ecdef6b64191b16d44
appleGuard <appleGuard21@gmail.com>
2022-07-14 22:25:36 +0300

    add caching

--- src/main/java/com/appleguard/pharmacy_parser/PharmacyParserApplication.java
+++ src/main/java/com/appleguard/pharmacy_parser/PharmacyParserApplication.java
@@ -2,8 +2,10 @@ package com.appleguard.pharmacy_parser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class PharmacyParserApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/appleguard/pharmacy_parser/entity/Drug.java
+++ src/main/java/com/appleguard/pharmacy_parser/entity/Drug.java
@@ -1,10 +1,12 @@
 package com.appleguard.pharmacy_parser.entity;
 
 import lombok.Data;
+import org.springframework.data.redis.core.RedisHash;
 import org.springframework.stereotype.Component;
 
 @Component
 @Data
+@RedisHash("Drug")
 public class Drug {
      int id;
      String fullName;
